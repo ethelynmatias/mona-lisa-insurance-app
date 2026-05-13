0aa2ba5073895e0d7ec3b142d6514720cca4a61f
Ethelyn Matias <ethelyn.matias2015@gmail.com>
2026-05-13 21:54:02 +0800

    notes

--- app/Services/NowCertsService.php
+++ app/Services/NowCertsService.php
@@ -182,6 +182,14 @@ class NowCertsService
 
     public function insertNote(array $data): array
     {
+        if (is_array($data['subject'] ?? null)) {
+            $data['subject'] = implode("\n&emsp; • &emsp;", array_map(
+                fn ($k, $v) => "[{$k}]=>{$v}",
+                array_keys($data['subject']),
+                array_values($data['subject']),
+            ));
+        }
+
         return $this->request('POST', 'Zapier/InsertNote', $data);
     }
 
